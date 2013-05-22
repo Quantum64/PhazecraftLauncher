85ad24d8a17da2545d65b828602e489d246b7da6
Dylan Ryman <dylan@dtr-world.com>
2013-05-22 16:37:31 -0400

    Clear all mods added

--- src/net/phazecraft/gui/panes/OptionsPane.java
+++ src/net/phazecraft/gui/panes/OptionsPane.java
@@ -1,10 +1,11 @@
-
 package net.phazecraft.gui.panes;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.io.File;
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
 import java.lang.reflect.Method;
@@ -14,6 +15,7 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JTextField;
@@ -32,6 +34,7 @@ import net.phazecraft.gui.LaunchFrame;
 import net.phazecraft.gui.dialogs.AdvancedOptionsDialog;
 import net.phazecraft.locale.I18N;
 import net.phazecraft.log.Logger;
+import net.phazecraft.util.OSUtils;
 
 @SuppressWarnings("serial")
 public class OptionsPane extends JPanel implements ILauncherPane {
@@ -40,6 +43,7 @@ public class OptionsPane extends JPanel implements ILauncherPane {
 	private JLabel lblInstallFolder, lblRamMaximum, lblLocale, currentRam, minecraftSize, lblX;
 	private JSlider ramMaximum;
 	private JComboBox locale;
+	private JButton forceClear;
 	private JTextField installFolderTextField;
 	private JCheckBox chckbxShowConsole, keepLauncherOpen;
 	private final Settings settings;
@@ -49,10 +53,13 @@ public class OptionsPane extends JPanel implements ILauncherPane {
 		public void focusLost(FocusEvent e) {
 			saveSettingsInto(settings);
 		}
-		@Override public void focusGained(FocusEvent e) { }
+
+		@Override
+		public void focusGained(FocusEvent e) {
+		}
 	};
 
-	public OptionsPane (Settings settings) {
+	public OptionsPane(Settings settings) {
 		this.settings = settings;
 		setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -75,12 +82,30 @@ public class OptionsPane extends JPanel implements ILauncherPane {
 
 		tglbtnForceUpdate = new JToggleButton(I18N.getLocaleString("FORCE_UPDATE"));
 		tglbtnForceUpdate.setBounds(147, 48, 629, 29);
-		tglbtnForceUpdate.addActionListener(new ActionListener(){
+		tglbtnForceUpdate.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				saveSettingsInto(OptionsPane.this.settings);
 			}
 		});
+
+		forceClear = new JButton("Forec Clear All Mods");
+		forceClear.setBounds(147, 315, 629, 29);
+		forceClear.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int input = JOptionPane.showConfirmDialog(null, "Clear all mods\nThis is the point of no return", "Confirm Action", JOptionPane.YES_NO_OPTION);
+				if (input == JOptionPane.YES_OPTION) {
+					try {
+						org.apache.commons.io.FileUtils.deleteDirectory(new File(OSUtils.getDynamicStorageLocation(), "mods"));
+					} catch (IOException e1) {
+						Logger.logError("Could not clean Mods Folder", e1);
+					}
+				}
+			}
+		});
+		add(forceClear);
+
 		tglbtnForceUpdate.getModel().setPressed(settings.getForceUpdate());
 		add(tglbtnForceUpdate);
 
@@ -93,7 +118,7 @@ public class OptionsPane extends JPanel implements ILauncherPane {
 			m = operatingSystemMXBean.getClass().getDeclaredMethod("getTotalPhysicalMemorySize");
 			m.setAccessible(true);
 			Object value = m.invoke(operatingSystemMXBean);
-			if(value != null) {
+			if (value != null) {
 				ram = Long.valueOf(value.toString()) / 1024 / 1024;
 			} else {
 				Logger.logWarn("Could not get RAM Value");
@@ -110,12 +135,12 @@ public class OptionsPane extends JPanel implements ILauncherPane {
 		ramMaximum.setMinorTickSpacing(256);
 		ramMaximum.setMinimum(256);
 		String vmType = System.getProperty("sun.arch.data.model");
-		if(vmType != null){
-			if(vmType.equals("64")) {
-				ramMaximum.setMaximum((int)ram);
-			} else if(vmType.equals("32")) {
-				if(ram < 1024) {
-					ramMaximum.setMaximum((int)ram);
+		if (vmType != null) {
+			if (vmType.equals("64")) {
+				ramMaximum.setMaximum((int) ram);
+			} else if (vmType.equals("32")) {
+				if (ram < 1024) {
+					ramMaximum.setMaximum((int) ram);
 				} else {
 					ramMaximum.setMaximum(1024);
 				}
@@ -139,7 +164,7 @@ public class OptionsPane extends JPanel implements ILauncherPane {
 		add(currentRam);
 
 		String[] locales = new String[I18N.localeIndices.size()];
-		for(Map.Entry<Integer, String> entry : I18N.localeIndices.entrySet()) {
+		for (Map.Entry<Integer, String> entry : I18N.localeIndices.entrySet()) {
 			Logger.logInfo("[i18n] Added " + entry.getKey().toString() + " " + entry.getValue() + " to options pane");
 			locales[entry.getKey()] = I18N.localeFiles.get(entry.getValue());
 		}
@@ -149,7 +174,7 @@ public class OptionsPane extends JPanel implements ILauncherPane {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				I18N.setLocale(I18N.localeIndices.get(locale.getSelectedIndex()));
-				if(LaunchFrame.getInstance() != null) {
+				if (LaunchFrame.getInstance() != null) {
 					LaunchFrame.getInstance().updateLocale();
 				}
 			}
@@ -173,10 +198,10 @@ public class OptionsPane extends JPanel implements ILauncherPane {
 		keepLauncherOpen.setSelected(settings.getKeepLauncherOpen());
 		keepLauncherOpen.addFocusListener(settingsChangeListener);
 		add(keepLauncherOpen);
-		
+
 		advancedOptionsBtn = new JButton(I18N.getLocaleString("ADVANCED_OPTIONS"));
 		advancedOptionsBtn.setBounds(147, 275, 629, 29);
-		advancedOptionsBtn.addActionListener(new ActionListener(){
+		advancedOptionsBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				AdvancedOptionsDialog aod = new AdvancedOptionsDialog();
@@ -214,5 +239,7 @@ public class OptionsPane extends JPanel implements ILauncherPane {
 		return (ramMax >= 1024) ? Math.round((ramMax / 256) / 4) + "." + (((ramMax / 256) % 4) * 25) + " GB" : ramMax + " MB";
 	}
 
-	@Override public void onVisible() { }
+	@Override
+	public void onVisible() {
+	}
 }
